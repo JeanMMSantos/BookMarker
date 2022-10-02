00b8faeee661f8b634d082dc3d65ea940329a413
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-10-02 01:31:28 -0300

    Initial commit

---
+++ src/main/java/com/company/bookmarker/MarcadorLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.company.bookmarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcadorLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcadorLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/marcadorlivros/MarcadorLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.marcadorlivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcadorLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
