6b05a52b8082048690201c52ef6426b771d0b160
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-10-02 01:40:28 -0300

    Application Name

--- src/main/java/com/company/bookmarker/BookMarkerApplication.java
+++ src/main/java/com/company/bookmarker/BookMarkerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarcadorLivrosApplication {
+public class BookMarkerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarcadorLivrosApplication.class, args);
+		SpringApplication.run(BookMarkerApplication.class, args);
 	}
 
 }
