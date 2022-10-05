7972947ec30dc38c24fd27ebfaafe69a3ced447b
JoseCarlos67 <carlos6767jose@gmail.com>
2022-10-05 20:02:10 -0300

    Adicionado a classe BookApi

---
+++ src/main/java/com/company/bookmarker/entities/users/BookApi.java
@@ -0,0 +1,28 @@
+package com.company.bookmarker.entities.users;
+
+public class BookApi {
+
+    private String key;
+    private String isbn;
+
+    public BookApi(String key, String isbn) {
+        this.key = key;
+        this.isbn = isbn;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
