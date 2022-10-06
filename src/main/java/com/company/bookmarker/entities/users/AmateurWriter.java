51b58c3e2140301331f6e5bcf1b4d28fe39523e9
YuriTartarini <114184611+YuriTartarini@users.noreply.github.com>
2022-10-06 15:07:24 -0300

    Update AmateurWriter.java

---
+++ src/main/java/com/company/bookmarker/entities/users/AmateurWriter.java
@@ -0,0 +1,20 @@
+
+package com.company.bookmarker.entities.users;
+
+import java.time.LocalDate;
+
+
+
+public class AmateurWriter extends User {
+    private String pseudonym;
+    private String Description;
+    private String mostPopBook; //futuramente sera da classe book
+    private String writedBooks; //futuramente sera da classe book
+    
+
+    public AmateurWriter() {
+
+    }
+
+    
+}
