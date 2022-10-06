2a42b6327f10772d3bc288f2354ef31b5b2a0c73
YuriTartarini <114184611+YuriTartarini@users.noreply.github.com>
2022-10-06 16:47:58 -0300

    Update AmateurWriter.java

--- src/main/java/com/company/bookmarker/entities/users/AmateurWriter.java
+++ src/main/java/com/company/bookmarker/entities/users/AmateurWriter.java
@@ -1,6 +1,7 @@
 
 package com.company.bookmarker.entities.users;
 
+import com.company.bookmarker.entities.libraries.AmateurWriterBook;
 import java.time.LocalDate;
 
 
@@ -8,8 +9,8 @@ import java.time.LocalDate;
 public class AmateurWriter extends User {
     private String pseudonym;
     private String Description;
-    private String mostPopBook; //futuramente sera da classe book
-    private String[] writedBooks; //futuramente sera da classe book
+    private AmateurWriterBook mostPopBook; 
+    private AmateurWriterBook[] writedBooks;
     
 
     public AmateurWriter() {
