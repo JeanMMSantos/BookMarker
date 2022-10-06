b36766ce7bd10a7a2f193e5e36d9bad0d41c380a
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-10-05 22:44:42 -0300

    Change "User" to an Abstract Class

--- src/main/java/com/company/bookmarker/entities/users/user.java
+++ src/main/java/com/company/bookmarker/entities/users/user.java
@@ -2,7 +2,7 @@ package com.company.bookmarker.entities.users;
 
 import java.time.LocalDate;
 
-public class User {
+public abstract class User {
     private String name, password, email, gender, photo;
     private LocalDate date;
     private Long cpf, phone;
