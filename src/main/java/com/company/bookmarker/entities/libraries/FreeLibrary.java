1bdbc2a7c51f80251637ca16b7ab6c966d2dfbfc
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-10-06 23:45:13 -0300

    Update Libraries

--- src/main/java/com/company/bookmarker/entities/libraries/FreeLibrary.java
+++ src/main/java/com/company/bookmarker/entities/libraries/FreeLibrary.java
@@ -1,7 +1,6 @@
 package com.company.bookmarker.entities.libraries;
-import java.util.ArrayList;
+
 public class FreeLibrary extends Library{
-    private ArrayList<BookStatus> defaultFreeLibrary = new ArrayList<BookStatus>();
 
     public FreeLibrary() {
     }
--- src/main/java/com/company/bookmarker/entities/libraries/Library.java
+++ src/main/java/com/company/bookmarker/entities/libraries/Library.java
@@ -1,4 +1,8 @@
 package com.company.bookmarker.entities.libraries;
 
+import java.util.ArrayList;
+
 public abstract class Library {
+
+    private ArrayList<BookStatus> defaultFreeLibrary = new ArrayList<BookStatus>();
 }
