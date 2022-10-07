16a4c352e42fbc903aac8f221deb7d645b3e8cc5
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-10-06 23:31:06 -0300

    Refactor

--- src/main/java/com/company/bookmarker/entities/libraries/PremiumLibrary.java
+++ src/main/java/com/company/bookmarker/entities/libraries/PremiumLibrary.java
@@ -6,11 +6,4 @@ public class PremiumLibrary extends Library{
     private String theme;
     private int id;
     private String favorite;
-<<<<<<< HEAD:src/main/java/com/company/bookmarker/entities/users/PremiumLibrary.java
-    private ArrayList<BookStatus> defaultPremiumLibrary = new ArrayList<BookStatus>();
-
-    public PremiumLibrary() {
-    }
-=======
->>>>>>> 4144c5e4bc9f8a076359d1bb4419aee668e3c061:src/main/java/com/company/bookmarker/entities/libraries/PremiumLibrary.java
 }
--- src/main/java/com/company/bookmarker/entities/users/Payment.java
+++ src/main/java/com/company/bookmarker/entities/users/Payment.java
@@ -1,9 +1,5 @@
 package com.company.bookmarker.entities.users;
 
-<<<<<<< HEAD:src/main/java/com/company/bookmarker/entities/users/Library.java
-public abstract class Library {
-=======
 public interface Payment {
     
->>>>>>> 4144c5e4bc9f8a076359d1bb4419aee668e3c061:src/main/java/com/company/bookmarker/entities/users/Payment.java
 }
