ca566685f8acb20f9491fdbe4cd54802c7ca61b9
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-10-06 23:33:30 -0300

    Update AmateurWriterBook.java

--- src/main/java/com/company/bookmarker/entities/libraries/AmateurWriterBook.java
+++ src/main/java/com/company/bookmarker/entities/libraries/AmateurWriterBook.java
@@ -5,24 +5,6 @@ public class AmateurWriterBook {
     private int id;
     private double bookValue;
 
-    public AmateurWriterBook(int id, double bookValue) {
-        this.id = id;
-        this.bookValue = bookValue;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public double getBookValue() {
-        return bookValue;
-    }
-
-    public void setBookValue(double bookValue) {
-        this.bookValue = bookValue;
+    public AmateurWriterBook() {
     }
 }
