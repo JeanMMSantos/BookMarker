df08a4e399b2cc65a637d5d94a914e42e42bb4c3
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-11-19 17:21:59 -0300

    Update AmateurWriter.java

--- src/main/java/com/company/bookmarker/entities/users/AmateurWriter.java
+++ src/main/java/com/company/bookmarker/entities/users/AmateurWriter.java
@@ -3,12 +3,19 @@ package com.company.bookmarker.entities.users;
 
 import com.company.bookmarker.entities.libraries.AmateurWriterBook;
 import java.util.ArrayList;
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 
 
 @Entity
 public class AmateurWriter extends User {
+
+    @OneToMany(mappedBy = "amateurWriterBook")
+    Set<AmateurWriterBook> amateurWriterBooks;
+
     private String pseudonym;
     private String Description;
     private AmateurWriterBook mostPopBook;
