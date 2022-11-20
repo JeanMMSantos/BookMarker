095d65bfec21e7cdbfe8d8395cb73430c3f0e014
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-11-19 21:40:13 -0300

    Update PaidUser.java

--- src/main/java/com/company/bookmarker/entities/users/PaidUser.java
+++ src/main/java/com/company/bookmarker/entities/users/PaidUser.java
@@ -8,19 +8,33 @@ import com.company.bookmarker.entities.libraries.FreeLibrary;
 import com.company.bookmarker.entities.libraries.PremiumLibrary;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import org.apache.tomcat.jni.Library;
 
 @Entity
 public class PaidUser extends User{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
     @OneToMany
-    @JoinColumn(name = "id")
-    Set<PremiumLibrary> premiumLibraries;
+    @JoinColumn(name = "paidUser_id")
+    private Set<PremiumLibrary> premiumLibraries;
+
+    @OneToOne
+    private FreeLibrary freeLibrary;
+    
 
     private int paymentTypeNumber;
     private float paymentValue;
-    private FreeLibrary freeLibrary;
+    //private FreeLibrary freeLibrary;
     //private ArrayList<PremiumLibrary> premiumLibraries = new ArrayList<PremiumLibrary>();
     private PaymentType paymentType;
 
