7f598ca718eff5be0a85211340e85bf474f6d56e
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-11-19 21:47:20 -0300

    Update FreeUser.java

--- src/main/java/com/company/bookmarker/entities/users/FreeUser.java
+++ src/main/java/com/company/bookmarker/entities/users/FreeUser.java
@@ -5,11 +5,22 @@ import java.time.LocalDate;
 import com.company.bookmarker.entities.libraries.FreeLibrary;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class FreeUser extends User{
 
-    private FreeLibrary freeLibrary;    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToOne
+    private FreeLibrary freeLibrary;
+
+    //private FreeLibrary freeLibrary;    
     //Constructors
     public FreeUser() {
     }
