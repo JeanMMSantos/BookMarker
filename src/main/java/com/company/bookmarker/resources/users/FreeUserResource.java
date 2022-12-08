ac8388a153ce87f8c378da6a6b307f23d963164d
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-12-08 01:08:28 -0300

    CRUD - FreeUser

--- src/main/java/com/company/bookmarker/resources/users/FreeUserResource.java
+++ src/main/java/com/company/bookmarker/resources/users/FreeUserResource.java
@@ -6,6 +6,7 @@ import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,4 +43,10 @@ public class FreeUserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(freeUser.getId()).toUri();
         return ResponseEntity.created(uri).body(freeUser);
     }
+
+    @DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		freeUserService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/company/bookmarker/services/users/FreeUserService.java
+++ src/main/java/com/company/bookmarker/services/users/FreeUserService.java
@@ -2,6 +2,8 @@ package com.company.bookmarker.services.users;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.company.bookmarker.entities.users.FreeUser;
 import com.company.bookmarker.repositories.users.FreeUserRepository;
@@ -25,5 +27,24 @@ public class FreeUserService {
         return freeUserRepository.save(freeUser);
     }
 
+    public void delete(Long id) {
+		freeUserRepository.deleteById(id);
+	}
     
+    public FreeUser update(Long id, FreeUser freeUser){
+        @SuppressWarnings("deprecation")
+        FreeUser entity = freeUserRepository.getById(id);
+        updateData(entity, freeUser);
+        return freeUserRepository.save(entity);
+    }
+
+    private void updateData(FreeUser entity, FreeUser freeUser){
+        entity.setCpf(freeUser.getCpf());
+        entity.setDate(freeUser.getDate());
+        entity.setEmail(freeUser.getEmail());
+        entity.setGender(freeUser.getGender());
+        entity.setFreeLibrary(freeUser.getFreeLibrary());
+        entity.setName(freeUser.getName());
+        entity.setPassword(freeUser.getPassword());
+    }
 }
