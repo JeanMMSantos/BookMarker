0fa0d01bccfb43f9a9cc8a0279408d32bdb2a6a8
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-12-07 23:34:56 -0300

    Update FreeUserResource.java

--- src/main/java/com/company/bookmarker/resources/users/FreeUserResource.java
+++ src/main/java/com/company/bookmarker/resources/users/FreeUserResource.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.company.bookmarker.entities.users.FreeUser;
+import com.company.bookmarker.repositories.users.FreeUserRepository;
 import com.company.bookmarker.services.users.FreeUserService;
 
 @RestController
@@ -27,5 +30,10 @@ public class FreeUserResource {
     @GetMapping(value = "{id}")
     public ResponseEntity<FreeUser> findById(@PathVariable Long id){
         return ResponseEntity.ok(freeUserService.findById(id));
+    }
+
+    @PostMapping
+    public FreeUser create(@RequestBody FreeUser freeUser){
+        return FreeUserRepository.save();
     }
 }
