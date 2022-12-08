5fd102e2db11b7f1aa7b9601b4efd1efb10fb40e
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-12-08 01:22:39 -0300

    Update PaidUserResource.java

--- src/main/java/com/company/bookmarker/resources/users/PaidUserResource.java
+++ src/main/java/com/company/bookmarker/resources/users/PaidUserResource.java
@@ -7,6 +7,7 @@ import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,4 +43,10 @@ public class PaidUserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(paidUser.getId()).toUri();
         return ResponseEntity.created(uri).body(paidUser);
     }
+
+    @DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		paidUserService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
