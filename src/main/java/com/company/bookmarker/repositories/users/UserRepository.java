0c86e808fc8cbf8a3862865927f1101b6fc1782d
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-12-08 01:10:44 -0300

    Delete - User

--- src/main/java/com/company/bookmarker/repositories/users/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.bookmarker.repositories.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.company.bookmarker.entities.users.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-    
-}
