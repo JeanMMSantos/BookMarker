1f529564b5c1fa102567de2e046d737f00d3f53b
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-12-08 01:30:49 -0300

    CRUD

--- src/main/java/com/company/bookmarker/services/users/AmauterWriterService.java
+++ src/main/java/com/company/bookmarker/services/users/AmauterWriterService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.company.bookmarker.entities.users.AmateurWriter;
 import com.company.bookmarker.repositories.users.AmauterWriterRepository;
+import com.company.bookmarker.repositories.users.PaidUserRepository;
+
 import java.util.List;
 
 @Service
@@ -18,4 +20,12 @@ public class AmauterWriterService {
     public AmateurWriter findById(long id){
         return amauterWriterRepository.findById(id).get();
     }
+
+    public AmateurWriter save(AmateurWriter amateurWriter){
+        return AmauterWriterRepository.save(amateurWriter);
+    }
+
+    public void delete(Long id) {
+		AmauterWriterRepository.deleteById(id);
+	}
 }
