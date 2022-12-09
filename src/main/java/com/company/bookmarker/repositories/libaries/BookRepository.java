520495590395f9eeec4f298ceb9ace1dec5e57c5
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-12-09 19:59:36 -0300

    Delete BookRepository.java

--- src/main/java/com/company/bookmarker/repositories/libaries/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.bookmarker.repositories.libaries;
-
-import com.company.bookmarker.entities.libaries.AmateurWriterBook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<AmateurWriterBook, Long> {
-}
