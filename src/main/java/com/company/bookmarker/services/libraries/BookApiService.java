6fc27e13fc6ae220d90402012526af9f7ee03ae3
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-12-10 10:27:57 -0300

    Update BookApiService.java

--- src/main/java/com/company/bookmarker/services/libraries/BookApiService.java
+++ src/main/java/com/company/bookmarker/services/libraries/BookApiService.java
@@ -1,6 +1,6 @@
 package com.company.bookmarker.services.libraries;
 
-import com.company.bookmarker.entities.libraries.AmateurWriterBook;
+import com.company.bookmarker.entities.libraries.Book;
 import com.company.bookmarker.entities.libraries.BookApi;
 import com.company.bookmarker.repositories.libraries.BookApiRepository;
 import org.springframework.stereotype.Service;
@@ -13,11 +13,11 @@ public class BookApiService {
 
     public BookApiRepository bookApiRepository;
 
-    public List<AmateurWriterBook> findAll() {
+    public List<Book> findAll() {
         return bookApiRepository.findAll();
     }
 
-    public AmateurWriterBook findById(Long id) {
+    public BookApi findById(Long id) {
         return bookApiRepository.findById(id).get();
     }
 
@@ -30,6 +30,7 @@ public class BookApiService {
     }
 
     public BookApi update(Long id, BookApi bookApi) {
+        @SuppressWarnings("depreciation")
         BookApi entity = bookApiRepository.getReferenceById(id);
         updateData(entity, bookApi);
         return bookApiRepository.save(entity);
