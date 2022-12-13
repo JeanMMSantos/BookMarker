9c1b1fcac04c1cff757d5fc2d3dd2bb7a7aad4e6
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-12-12 21:42:50 -0300

    Update PaidUserResource.java

--- src/main/java/com/company/bookmarker/resources/users/PaidUserResource.java
+++ src/main/java/com/company/bookmarker/resources/users/PaidUserResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,4 +50,10 @@ public class PaidUserResource {
 		paidUserService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<PaidUser> updated(@PathVariable Long id, @RequestBody PaidUser paidUser) {
+        paidUser = paidUserService.update(id, paidUser);
+        return ResponseEntity.ok(paidUser);
+    }
 }
