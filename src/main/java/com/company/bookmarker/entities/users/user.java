b106d51a5f63eb8042c04167dc797ec35a76ece6
JeanMMSantos <99457844+JeanMMSantos@users.noreply.github.com>
2022-12-12 22:29:02 -0300

    Update user.java

--- src/main/java/com/company/bookmarker/entities/users/user.java
+++ src/main/java/com/company/bookmarker/entities/users/user.java
@@ -1,9 +1,12 @@
 package com.company.bookmarker.entities.users;
 import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="TBUser")
@@ -13,12 +16,23 @@ public abstract class user {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @Column(length=70)
     private String name;
+
+    @Column(length=70)
     private String password;
+
+    @Column(length=70)
     private String email;
+
+    @Column(length=70)
     private String gender;
+
+    @Column(length=70)
     private String photo;
+
     private LocalDate date;
+
     private Long cpf, phone;
     
     //Constructors
